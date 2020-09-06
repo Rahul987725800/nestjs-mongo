09e54eca3cb31d154928d568c0a4509d7880f5e8
Rahul Gupta <guptarahul70322@gmail.com>
2020-09-06 14:31:10 +0530

    add a product and get all products

---
+++ src/products/products.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { ProductsService } from './products.service';
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+  @Post()
+  addProduct(
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc,
+    @Body('price') prodPrice: number,
+  ) {
+    const generatedId = this.productsService.insertProduct(prodTitle, prodDesc, prodPrice);
+    return {id : generatedId};
+  }
+  @Get()
+  getAllProducts() {
+    return this.productsService.getProducts();
+  }
+}
